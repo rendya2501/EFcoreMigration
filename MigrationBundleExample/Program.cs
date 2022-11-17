0903e059411be3d8ea0942870ce7f89da9853f9d
伊藤 駿 <rendya2501@gmail.com>
2022-11-17 18:32:50 +0900

    ・プロジェクト整理 ・OnConfigure動作検証

--- MigrationBundleExample/Program.cs
+++ MigrationBundleExample/Program.cs
@@ -17,21 +17,27 @@ class Program
         //ServiceProvider serviceProvider = services.BuildServiceProvider();
         //_datContext = serviceProvider.GetService<DatContext>();
 
-        
-        // ○ bundle作成できた。
+
+        // ○ DIでbundle作成できたパターン
         // ASP.NET Core 2.2 アプリで dotnet ef コマンドを実行する場合は、 Program.cs に CreateWebHostBuilder メソッドが必要な模様
         // https://qiita.com/wukann/items/53462f4b21104ed75c31
-        using IHost host = Host.CreateDefaultBuilder(args)
-            .ConfigureServices((hostContext, services) =>
-            {
-                services
-                    .AddDbContext<DatContext>(options =>
-                    {
-                        var appsettings = hostContext.Configuration.GetConnectionString("DefaultConnection");
-                        options.UseSqlServer(appsettings ?? connectionString);
-                    });
-            })
-            .Build();
+        //using IHost host = Host.CreateDefaultBuilder(args)
+        //    .ConfigureServices((hostContext, services) =>
+        //    {
+        //        services
+        //            .AddDbContext<DatContext>(options =>
+        //            {
+        //                var appsettings = hostContext.Configuration.GetConnectionString("DefaultConnection");
+        //                options.UseSqlServer(appsettings ?? connectionString);
+        //            });
+        //    })
+        //    .Build();
+
+
+        // Contextクラスにおいて直接、接続情報を記述した場合のMigrate実行
+        Console.WriteLine("開始");
+        using var datContext = new DatContext();
+        datContext.Database.Migrate();
     }
 }
 
